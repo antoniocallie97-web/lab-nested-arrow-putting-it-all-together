5a88567610133aeea4237798496eceb7e161489a
antoniocallie97-web <antoniocallie97@gmail.com>
2026-04-20 14:15:07 +0300

    lab-nested-arrow-putting-it-all-together

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ function createLoginTracker(userInfo) {
   return (passwordAttempt) => {
     attemptCount++;
 
+    // Lock AFTER 3 attempts (on 4th call)
     if (attemptCount > 3) {
       return "Account locked due to too many failed login attempts";
     }
 
+    // Correct password
     if (passwordAttempt === userInfo.password) {
       return "Login successful";
     }
 
+    // Failed attempt
     return `Attempt ${attemptCount}: Login failed`;
   };
 }
